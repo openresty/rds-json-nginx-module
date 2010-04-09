98b2b08fb0b17ad1657d03274184e7678e3aaa6d
agentzh (章亦春) <agentzh@gmail.com>
2010-04-10 01:59:14 +0800

    fixed a nasty bug in ngx_http_rds_json_discard_bufs which may produce segfault or hang.

--- src/ngx_http_rds_json_util.c
+++ src/ngx_http_rds_json_util.c
@@ -133,7 +133,7 @@ ngx_http_rds_json_discard_bufs(ngx_pool_t *pool, ngx_chain_t *in)
 {
     ngx_chain_t         *cl;
 
-    for (cl = in->next; cl; cl = cl->next) {
+    for (cl = in; cl; cl = cl->next) {
         if (cl->buf->temporary && cl->buf->memory
                 && ngx_buf_size(cl->buf) > 0) {
             ngx_pfree(pool, cl->buf->start);
