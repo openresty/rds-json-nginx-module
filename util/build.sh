84b5222db68b06d9fd5d3219235af5636b8e2ef5
agentzh (章亦春) <agentzh@gmail.com>
2011-12-05 17:12:51 +0800

    use the ngx-build script from the nginx-devel-utils project to build everything in our developer build script.

--- util/build.sh
+++ util/build.sh
@@ -3,38 +3,12 @@
 # this file is mostly meant to be used by the author himself.
 
 root=`pwd`
-version=$1
 home=~
+version=$1
+force=$2
 #opts=$2
-target=$root/work/nginx
 
-if [ ! -d ./buildroot ]; then
-    mkdir ./buildroot || exit 1
-fi
-
-cd buildroot || exit 1
-
-if [ ! -s "nginx-$version.tar.gz" ]; then
-    if [ -f ~/work/nginx-$version.tar.gz ]; then
-        cp ~/work/nginx-$version.tar.gz ./ || exit 1
-    else
-        wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
-        cp nginx-$version.tar.gz ~/work/ || exit 1
-    fi
-
-    tar -xzvf nginx-$version.tar.gz || exit 1
-    cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./ || exit 1
-    patch -p0 < nginx-$version-no_pool.patch || exit 1
-fi
-
-#tar -xzvf nginx-$version.tar.gz || exit 1
-#cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./ || exit 1
-#patch -p0 < nginx-$version-no_pool.patch || exit 1
-
-cd nginx-$version/ || exit 1
-
-if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
-    ./configure --prefix=$target \
+ngx-build $force $version \
           --with-cc-opt="-O1" \
             --with-ld-opt="-Wl,-rpath,/opt/drizzle/lib:/opt/pg9/lib" \
             --without-mail_pop3_module \
@@ -57,26 +31,11 @@ if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$
           --add-module=$root/../drizzle-nginx-module \
           --add-module=$root/../form-input-nginx-module \
           --add-module=$root/../postgres-nginx-module \
-          --with-debug \
-        || exit 1
+          --with-debug
           #--add-module=$root/../lua-nginx-module \
           #--add-module=$home/work/ngx_http_auth_request-0.1 #\
           #--with-rtsig_module
           #--with-cc-opt="-g3 -O0"
           #--add-module=$root/../echo-nginx-module \
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
-    if [ $? -ne 0 ]; then
-        echo "Failed to configure"
-        exit 1
-    fi
-fi
-
-if [ -f $target/sbin/nginx ]; then
-    rm -f $target/sbin/nginx
-fi
-if [ -f $target/logs/nginx.pid ]; then
-    kill `cat $target/logs/nginx.pid`
-fi
-make -j3
-make install
 
